23bad9b9a9a7a14d5e864cb5713fb2562e5665f4
Inkounter <inkounter@gmail.com>
2021-06-06 15:48:58 -0400

    Rename methods and data members intended to be private, and add contracts.

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 local thisAddonName, namespace = ...
 
 local logMessage = function(message)
+    -- Log the specified 'message' with an addon-specific prefix.
+
     print(string.format("|cFF5555BB%s:|r %s", thisAddonName, message))
 end
 
@@ -14,38 +16,53 @@ local defaultEnabledDifficultyIds = {
 local enabledDifficultyIds = defaultEnabledDifficultyIds
 
 local CombatLoggerMixin = {
-    ["loggingEnabled"] = false,
+    ["_loggingEnabled"] = false,
 
-    ["enableLogging"] = function(self)
-        if self.loggingEnabled then
+    ["_enableLogging"] = function(self)
+        -- If combat logging is not already enabled, print a message and enable
+        -- it.  Otherwise, do nothing.
+
+        if self._loggingEnabled then
             return
         end
 
-        self.loggingEnabled = true
+        self._loggingEnabled = true
         logMessage("|cFF00FF00ENABLING|r combat logging.")
         LoggingCombat(true)
     end,
 
-    ["disableLogging"] = function(self)
-        if not self.loggingEnabled then
+    ["_disableLogging"] = function(self)
+        -- If combat logging is not already disabled, print a message and
+        -- disable it.  Otherwise, do nothing.
+
+        if not self._loggingEnabled then
             return
         end
 
-        self.loggingEnabled = false
+        self._loggingEnabled = false
         logMessage("|cFFFF0000DISABLING|r combat logging.")
         LoggingCombat(false)
     end,
 
-    ["UPDATE_INSTANCE_INFO"] = function(self)
+    ["_UPDATE_INSTANCE_INFO"] = function(self)
+        -- Check the current instance difficulty and enable or disable combat
+        -- logging accordingly.  Note that this handles the
+        -- "UPDATE_INSTANCE_INFO" event.
+
         local difficultyId = select(3, GetInstanceInfo())
         if enabledDifficultyIds[difficultyId] then
-            self:enableLogging()
+            self:_enableLogging()
         else
-            self:disableLogging()
+            self:_disableLogging()
         end
     end,
 
-    ["ADDON_LOADED"] = function(self, addonName)
+    ["_ADDON_LOADED"] = function(self, addonName)
+        -- If the specified 'addonName' is the name of this addon, then stop
+        -- listening for "ADDON_LOADED" events, load the saved variable, and
+        -- re-execute the enablement/disablement of combat logging.  Otherwise,
+        -- if 'addonName' is not the name of this addon, do nothing.
+
         if addonName ~= thisAddonName then
             return
         end
@@ -56,19 +73,33 @@ local CombatLoggerMixin = {
 
         DevTools_Dump(enabledDifficultyIds)
 
-        self:UPDATE_INSTANCE_INFO()
+        self:_UPDATE_INSTANCE_INFO()
     end,
 
-    ["registerEvents"] = function(self)
+    ["_registerEvents"] = function(self)
+        -- Invoke methods from the 'Frame' mixin to listen for API events and
+        -- to match those events to callbacks.
+
         self:RegisterEvent("UPDATE_INSTANCE_INFO")
         self:RegisterEvent("ADDON_LOADED")
 
         self:SetScript("OnEvent",
-                       function(self, event, ...) self[event](self, ...) end)
+                       function(self, event, ...) self["_" .. event](self, ...) end)
+    end,
+
+    ["_createOptions"] = function(self)
+        -- Create and attach the interface options frame.
+    end,
+
+    ["install"] = function(self)
+        -- Initialize this object.
+
+        self:_registerEvents()
+        self:_createOptions()
     end,
 }
 
 local frame = CreateFrame("Frame")
 Mixin(frame, CombatLoggerMixin)
 
-frame:registerEvents()
+frame:install()
